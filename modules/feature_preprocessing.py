db0b7552560aff7dab49c8a4b1a2c3830c818c2b
Damiano Clementel <damiano.clementel@gmail.com>
2019-06-12 10:09:32 +0200

    First completed version

--- modules/feature_preprocessing.py
+++ modules/feature_preprocessing.py
@@ -1,6 +1,9 @@
 # Required libraries
 import pandas as pd
 import logging as log
+import time
+import numpy as np
+from scipy import signal
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Given an iterable (list or Series), turns it into a bag of words matrix (DataFrame)
@@ -40,3 +43,76 @@ def replace_bow(df, col, vocabulary=None, prefix='', drop=False):
     df = pd.concat([df, bow], axis=1)
     # Return computed DataFrame
     return df
+
+
+"""
+Function to apply sliding windows on a proteins dataset. It uses Gaussian Filtering
+"""
+def sliding_window(data, k, sd):
+    """
+    REQUIRE:
+    import pandas as pd
+    import numpy as np
+    import signal from scipy
+
+    INPUT:
+    data = dataframe of main features
+    k = the size of a window (int)
+    sd = the standard deviation of the gaussian filter (float)
+
+    OUTPUT:
+    A dataframe with sliding windows applied
+    """
+
+    # Define starting time of the function
+    start = time.time()
+    #Set variables
+    df_windows = data.copy()
+
+    #Cycle for every protein
+    for pdb_id in data.PDB_ID.unique():
+        #Cycle for every chain in a given protein
+        for chain in set(data.CHAIN_ID[data.PDB_ID == pdb_id].unique()):
+
+            #Work on a reduced dataset: we apply sliding windows for every chain
+            df_sliced = df_windows[(data.PDB_ID == pdb_id)
+                                   & (data.CHAIN_ID == chain)]
+
+            # SET PDB_ID, CHIAN_ID and RES_ID to a separated df, we are not going to apply gaussian filter on them
+            info_sliced = df_sliced.iloc[:, 0:3]
+
+            #Shortcut name for lengths
+            chain_len = len(data.CHAIN_ID[(data.PDB_ID == pdb_id)
+                                        & (data.CHAIN_ID == chain)])
+
+            #Apply a symmatric mirroring at the start of the chain of size k//2
+            df_windows_start = pd.DataFrame(np.array(df_sliced.iloc[1:(k//2+1), ]),
+                                            index=np.arange(-k//2 + 1, 0, step = 1),
+                                            columns=list(data.columns)).sort_index()
+
+            #Apply a symmatric mirroring at the end of the chain of k//2
+
+            df_windows_end = pd.DataFrame(np.array(df_sliced.iloc[chain_len-(k//2 + 1):chain_len-1, ]),
+                                          index=np.arange(chain_len-1 + k//2,chain_len-1, step = -1),
+                                          columns=list(data.columns)).sort_index()
+
+            #Now we merge reunite this dataframe
+            df_with_start_sym = df_windows_start.append(df_sliced)
+            df_win_k = df_with_start_sym.append(df_windows_end)
+
+            ### MAIN: COMPUTE GAUSSIAN FILTER OF GIVEN DATAFRAME
+            sliced = df_win_k.iloc[:, 3:]
+            window = signal.gaussian(k, std = sd)
+            sliced = sliced.rolling(window = k, center = True).apply(lambda x: np.dot(x,window)/k)
+
+            # Reunite filtered features with PDB_ID, CHAIN_ID, RES_ID
+            tot_sliced = pd.merge(info_sliced, sliced.iloc[0:chain_len+k//2,:],
+                                  right_index=True, left_index=True) #here is chain_len + k//2
+
+            ### Update the dataframe with the filtered features of given chain
+            df_windows[(df_windows.PDB_ID == pdb_id) & (df_windows.CHAIN_ID == chain)] = tot_sliced
+
+    # Debug time
+    log.debug('Window sliding took {}'.format(time.time() - start))
+    # Return "window slided" dataframe
+    return df_windows
--- train.py
+++ train.py
@@ -7,28 +7,30 @@ import logging
 import argparse
 # Import custom libraries
 from modules.feature_extraction import *
+from modules.feature_preprocessing import *
+from modules.pipelines import *
+
+# Set debugging level (default DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
 # Initialize configuration dictionary
 config = {
-    'config_file': './config.json',
-    'debug_level': 'DEBUG'
+    # Default configuration file
+    'config_file': './config.json'
 }
 
 # Load configuration settings from configuration file
-with open(config.config_file) as config_file:
+with open(config.get('config_file')) as config_file:
     config = json.load(config_file)
-
-# Set debugging level (default DEBUG)
-logging.basicConfig(level=logging[config.debug_level])
-# Debug config file
-logging.debug(config)
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='Train a given model to predict LIP flag over aminoacidic sequences', prog='LIP learner')
 # Define protein PDB ID
-parser.add_argument('pdb_id', '-pdb', '--pdb_id', help='PDB id of the protein', type=str, action='store')
+parser.add_argument('lip_file', help='FIle which contains LIP/non-LIP tagged aminoacidic sequence', type=str, action='store')
 # Define random seed
-parser.add_argument('-rs', --'random_seed', help='Random seed used in training', type=int, action='store')
+# parser.add_argument('-rs', --'random_seed', help='Random seed used in training', type=int, action='store')
+# Define PDB ids to be excluded by computations
+parser.add_argument('-e', '--exclude', help='PDB to be excluded from training', action='append')
 # Define window size
 parser.add_argument('-ws', '--window_size', help='Size of the window used to average residues features', type=int, action='store')
 # Define a force command do overwrite ring files
@@ -42,7 +44,9 @@ parser.add_argument('-of', '--out_file', help='Define in which file prediction r
 # Define configuration file to overwrite momentaniously overwrite the default one
 parser.add_argument('-cf', '--config_file', help='Define a custom configuration file, overwrites original parameters', type=str, action='store')
 # Parse arguments
-args = parser.parse_args()
+args = vars(parser.parse_args())
+# Delete arguments which are None
+args = {k: v for k, v in args.items() if v is not None}
 
 # Check if there is a configuration file specified
 new_config_file = args.get('config_file')
@@ -59,38 +63,7 @@ if new_config_file:
 config = {**config, **args}
 
 # Debug
-logging.debug(type(config))
-logging-debug(config)
+logging.debug(config)
 
-# Define a list of (1) PDB id
-pdb_ids = list(config.pdb_id)
-# Retrieve PDB file
-download_PDB(pdb_ids, pdb_dir=config.pdb_dir)
-# Retrieve PDB data from PDB files
-ds_residues = get_PDB(pdb_ids, pdb_dir=config.pdb_dir)
-
-# Retrieve DSSP features
-ds_dssp = get_DSSP(pdb_ids, pdb_dir=config.pdb_dir, dssp_path=config.dssp_path)
-# Merge DSSP features into residues dataset
-ds_residues = ds_residues.merge(ds_dssp, how='left', on=['PDB_ID', 'CHAIN_ID', 'RES_ID'])
-
-# Check if ring file has already been downloaded
-# Download RING files
-download_RING(pdb_ids, ring_dir=config.ring_dir)
-# Retrieve RING features
-ds_ring = get_RING(pdb_ids, pdb_dir=config.pdb_dir, ring_dir=config.ring_dir, contact_threshold=config.contact_threshold)
-# Compute edge features from RING dataset
-ds_RING_edges = get_RING_edges(ds_RING)
-# Merge edge features into residues dataset
-ds_residues = ds_residues.merge(ds_RING_edges, how='left', on=['PDB_ID', 'CHAIN_ID', 'RES_ID'])
-# Compute contact features from RING dataset
-ds_RING_contacts = get_RING_contacts(ds_RING)
-# Merge contact features into residues dataset
-ds_residues = ds_residues.merge(ds_RING_edges, how='left', on=['PDB_ID', 'CHAIN_ID', 'RES_ID'])
-# Move LIP_SCORE and LIP columns to the latest positions
-ds_residues = ds_residues.drop(columns=['LIP_SCORE', 'LIP']).assign(LIP_SCORE=ds_residues['LIP_SCORE'], LIP=ds_residues['LIP'])
-
-# Test on saved model
-
-
-# Merge features from dssp and ring into
+# Train the model
+train_pipeline(config)
